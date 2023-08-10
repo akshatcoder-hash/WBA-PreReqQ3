e379bd143e64c280e66c3710c846c5dd28d63df3
Akshat Sharma <akshatcoder@outlook.com>
2023-08-11 05:04:25 +0530

    prereqs done

---
+++ airdrop/programs/wba_prereq.ts
@@ -0,0 +1,16 @@
+export type WbaPrereq = {
+    version: string,
+    name: string,
+    instructions: Array<any>
+};
+
+export const IDL: WbaPrereq = {
+    version: "0.1.0",
+    name: "wba_prereq",
+    instructions: [
+        {
+            name: "complete",
+        
+        }
+    ]
+};
